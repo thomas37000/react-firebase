c542a97ca96895b96c92384eb2b3532cd7e68e1b
thomas <thomas.chalanson@sfr.fr>
2022-01-19 16:48:28 +0100

    do the logOut function

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,21 @@
 import React, { useContext } from "react";
 import { UserContext } from "../context/UserContext";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { signOut } from "firebase/auth";
+import { Auth } from "../firebase/config";
 
 const Navbar = () => {
   const { toggleModals } = useContext(UserContext);
+  const navigate = useNavigate()
+
+  const logout = async () => {
+    try {
+      await signOut(Auth)
+      navigate("/")
+    } catch {
+      alert("vider votre cache internet.")
+    }
+  }
 
   return (
     <>
@@ -26,7 +38,7 @@ const Navbar = () => {
           >
             Connexion
           </button>
-          <button type="button" className="btn btn-danger ms-2">
+          <button type="button" className="btn btn-danger ms-2" onClick={logout}>
             Déconnexion
           </button>
         </div>
